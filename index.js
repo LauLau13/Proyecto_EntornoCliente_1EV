38625b79af7d909a80f7539cd4e23d287ffc121e
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-23 20:57:33 +0100

    Funciones básicas para generar contraseñas y comprobar formularios

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const db = require("./db/models/index.js")
-const categories = require("./src/categories/category.controller.js")
+const db = require("./db/models/index.js");
+const categories = require("./src/categories/category.controller.js");
 
-
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-
-  // users.init(app)
-  categories.init(app)
-})
-
+  console.log(`Example app listening on port ${port}`);
+  categories.init(app);
+});
---
+++ www/Scripts/passSaver.js
@@ -0,0 +1,50 @@
+function generatePassword() {
+  // Genera una contraseña aleatoria de 9 caracteres
+  let password = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 10; i++) {
+    password += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  var field = document.getElementById("inputPassword");
+  field.value = password;
+}
+
+function hidePassword() {
+  var x = document.getElementById("inputPassword");
+  var eye = document.getElementById("showPassword");
+  if (x.type === "password") {
+    x.type = "text";
+    eye.innerHTML =
+      '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+  } else {
+    x.type = "password";
+    eye.innerHTML =
+      "<?xml version='1.0' encoding='UTF-8'?><svg width='24px' height='24px' stroke-width='1.5' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' color='#000000'><path d='M12 14a2 2 0 100-4 2 2 0 000 4z' stroke='#000000' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'></path><path d='M21 12c-1.889 2.991-5.282 6-9 6s-7.111-3.009-9-6c2.299-2.842 4.992-6 9-6s6.701 3.158 9 6z' stroke='#000000' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'></path></svg>";
+  }
+}
+
+function onCheckForm() {
+  /* validate email input has an email */
+  let alert =
+    "Todos los campos son obligatorios, por favor rellena todos los campos.";
+  let user = document.getElementById("inputUser").value;
+  if (user == "") {
+    console.log(alert);
+  }
+
+  /* validate password input has a password */
+  let password = document.getElementById("inputPassword").value;
+  if (password == "") {
+    console.log(alert);
+  }
+
+  /* validate url has an url */
+  let url = document.getElementById("inputURL").value;
+  if (url == "") {
+    console.log(alert);
+  }
+}
+
+function onsubmit() {}
