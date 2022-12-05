41e3d56769bfbe658fec08256d87f3a18df0b4e5
SANDARA\Sandra <a25660@svalero.com>
2022-12-05 17:54:53 +0100

    cambios

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -1,6 +1,32 @@
-const myModal = document.getElementById('exampleModal')
+/* const saveButton = document.getElementById('btSave');
+const cancelButton = document.getElementById('btCancel');
+const exampleModal = document.getElementById('exampleModal')
 const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
+//No funciona
+exampleModal.addEventListener('shown.bs.modal',  function () {
   myInput.focus()
 })
+
+// Save button opens a modal dialog
+saveButton.addEventListener('click', function() {
+  exampleModal.showModal();
+});
+
+// Cancel button closes the dialog box
+cancelButton.addEventListener('click', function() {
+  exampleModal.close();
+}); */
+
+if(document.getElementById("buttonModal")){
+  const modal = document.getElementById("myModal");
+  const button = document.getElementById("buttonModal");
+
+  modal.addEventListener('click', ()=>{
+    button.focus;
+  })
+
+  button.onclick = function(){
+    modal.style.display = "block";
+  
+  }
+}
